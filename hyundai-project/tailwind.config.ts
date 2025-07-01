b1278461d3e89319c4c34a8e4dc15634c750bc56
kimkyeongo <30765479+kimkyeongo@users.noreply.github.com>
2025-07-01 07:40:51 +0000

    commit

--- hyundai-project/tailwind.config.ts
+++ hyundai-project/tailwind.config.ts
@@ -2,19 +2,19 @@ import type { Config } from "tailwindcss"
 import defaultTheme from "tailwindcss/defaultTheme"
 
 const config: Config = {
-    darkMode: "class",
-    content: [
-        "./app/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}"
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
-            },
-        },
+  darkMode: "class",
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      },
     },
-    plugins: [require("tailwindcss-animate")],
+  },
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
